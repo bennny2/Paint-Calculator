84f9a9921dd651716116016618b0cfdfbeebc666
bennny2 <benhurley01@outlook.com>
2024-02-08 19:30:25 +0000

    Added CalculateFloorArea Method to RoomDimensions Class

--- RoomDimensions.cs
+++ RoomDimensions.cs
@@ -1,4 +1,14 @@
 public class RoomDimensions
 {
+    //Fields
 
+    private double floorArea;
+    private double roomVolume;
+   
+
+    //Methods
+
+    private static double CalculateFloorArea(double roomWidth, double roomLength) {
+        return roomWidth * roomLength;
+    }
 }
