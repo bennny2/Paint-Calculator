24becae7b6fbdcceba6c2870eaea799058793f0c
bennny2 <benhurley01@outlook.com>
2024-02-08 17:12:18 +0000

    Added test - Calculate Floor Area Test

--- Tests/RoomDimensionsTests.cs
+++ Tests/RoomDimensionsTests.cs
@@ -1,10 +1,22 @@
 using Xunit;
 
-
 namespace RoomDimensionsCalculator.Tests;
 
 public class RoomDimensionsTests
 {
+    [Fact]
+    public void CalculatorTests_CalculateFloorAreaWithValidInput_ReturnExpected() {
 
+        //Assign
+        double width = 10.0;
+        double length = 18.0;
+        double expectedFloorArea = 180.0;
+        RoomDimensions calculator = new();
+
+        //Act
+        double ouputtedArea = calculator.CalculateFloorArea(width, length); 
+
+        //Assert
+        Assert.Equal(ouputtedArea, expectedFloorArea);
+    }
 }
-
