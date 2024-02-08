83cc3967c119012e57f2ecc87f4bca0b90a7cd54
bennny2 <benhurley01@outlook.com>
2024-02-08 20:47:15 +0000

    Added neccessary variables into Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Variables
+RoomDimensions calculator = new();
+bool validWidth;
+bool validHeight;
+bool validLength;
+
+string? roomWidthInput;
+string? roomHeightInput;
+string? roomLengthInput;
+
+double roomWidth;
+double roomHeight;
+double roomLength;
+bool ceilingIncluded;
+
+double floorArea;
+double roomVolume;
+double paintNeeded;
+
+
