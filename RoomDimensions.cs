258a28dc368f5ac5befd6eb7e87f785aa16d1dd2
bennny2 <benhurley01@outlook.com>
2024-02-08 19:33:41 +0000

    Removed incorrect static modifier on CalculateFloorArea

--- RoomDimensions.cs
+++ RoomDimensions.cs
@@ -1,14 +1,8 @@
 public class RoomDimensions
 {
-    //Fields
-
-    private double floorArea;
-    private double roomVolume;
-   
-
     //Methods
 
-    private static double CalculateFloorArea(double roomWidth, double roomLength) {
+    public double CalculateFloorArea(double roomWidth, double roomLength) {
         return roomWidth * roomLength;
     }
 }
